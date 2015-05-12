aa5c3545719b94ccd6f61faba15455a42a8fe4a9
JackandRoses <zwf722@163.com>
2015-05-12 23:17:13 +0800

    省市县区信息

---
+++ src/com/zhouwf/myweather/util/Utility.java
@@ -0,0 +1,17 @@
+package com.zhouwf.myweather.util;
+
+import android.text.TextUtils;
+
+import com.zhouwf.myweather.db.MyWeatherDbOPerations;
+
+public class Utility {
+    public synchronized static boolean handleProvincesResponse(
+            MyWeatherDbOPerations myWeatherDbOP, String response) {
+        if (!TextUtils.isEmpty(response)) {
+
+        }
+
+        return false;
+
+    }
+}
