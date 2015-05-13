42edd34c27b2602a8c4e96a2e5370e79ce0cd13c
JackandRoses <zwf722@163.com>
2015-05-13 21:27:10 +0800

    test

--- src/com/zhouwf/myweather/util/Utility.java
+++ src/com/zhouwf/myweather/util/Utility.java
@@ -4,13 +4,16 @@ import android.text.TextUtils;
 
 import com.zhouwf.myweather.db.MyWeatherDbOPerations;
 
+/**
+ * @author zhouwf
+ * 
+ */
 public class Utility {
-    public synchronized static boolean handleProvincesResponse(
-            MyWeatherDbOPerations myWeatherDbOP, String response) {
+    public synchronized static boolean handleProvincesResponse(MyWeatherDbOPerations myWeatherDbOP, String response) {
         if (!TextUtils.isEmpty(response)) {
 
         }
-
+        ;
         return false;
 
     }
