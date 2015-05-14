b0b83d1de2b45a7194ffd8cd21b56e3a1620d54c
zhouwf <zhouwf@ldns-dt-1190.sunjapan.com.cn>
2015-05-14 21:31:31 +0800

    app launched

--- src/com/zhouwf/myweather/activity/ChooseAreaActivity.java
+++ src/com/zhouwf/myweather/activity/ChooseAreaActivity.java
@@ -56,6 +56,9 @@ public class ChooseAreaActivity extends Activity {
   /** currentLevel */
   private int currentLevel;
 
+  /** handle result */
+  private static boolean result = false;
+
   /*
    * (non-Javadoc)
    * 
@@ -79,15 +82,26 @@ public class ChooseAreaActivity extends Activity {
           selectedProvince = provinceList.get(position);
           try {
             queryCities();
-          } catch (IOException e) {
+          } catch (IOException | XmlPullParserException e) {
             e.printStackTrace();
-          } catch (XmlPullParserException e) {
+          }
+        } else if (currentLevel == LEVEL_CITY) {
+          selectedCity = cityList.get(position);
+          try {
+            queryCountries();
+          } catch (IOException | XmlPullParserException e) {
             e.printStackTrace();
           }
         }
+
       }
     });
-
+    // 启动时加载省级数据
+    try {
+      queryProvinces();
+    } catch (IOException | XmlPullParserException e) {
+      e.printStackTrace();
+    }
   }
 
   /**
@@ -114,6 +128,53 @@ public class ChooseAreaActivity extends Activity {
   }
 
   /**
+   * 查询所有省
+   * 
+   * @throws IOException
+   * @throws XmlPullParserException
+   */
+  protected void queryProvinces() throws IOException, XmlPullParserException {
+    provinceList = myWeatherDbOPerations.loadProvince();
+    if (provinceList != null && provinceList.size() > 0) {
+      dataList.clear();
+      for (Province eachProvince : provinceList) {
+        dataList.add(eachProvince.getProvinceName());
+      }
+      // 通知listView刷新
+      adapter.notifyDataSetChanged();
+      listView.setSelection(0);
+      titleText.setText("中国");
+      currentLevel = LEVEL_PROVINCE;
+    } else {
+      queryFromXml(null, 0, Districts.PROVINCE);
+    }
+  }
+
+  /**
+   * 查询所有区县
+   * 
+   * @throws IOException
+   * @throws XmlPullParserException
+   */
+  protected void queryCountries() throws IOException, XmlPullParserException {
+    countryList = myWeatherDbOPerations.loadCountry((selectedCity.getCityId()));
+    if (cityList != null && cityList.size() > 0) {
+      dataList.clear();
+      for (Country eachCountry : countryList) {
+        dataList.add(eachCountry.getCountryName());
+      }
+      // 通知listView刷新
+      adapter.notifyDataSetChanged();
+      listView.setSelection(0);
+      titleText.setText(selectedCity.getCityName());
+      currentLevel = LEVEL_COUNTRY;
+    } else {
+      queryFromXml(selectedCity.getCityCode(), selectedCity.getCityId(), Districts.COUNTRY);
+    }
+
+  }
+
+  /**
    * 从xml中查询省市数据
    * 
    * @param code 省、市CODE
@@ -130,21 +191,27 @@ public class ChooseAreaActivity extends Activity {
           switch (type) {
           case CITY:
             try {
-              Utility.handleCitiesResponse(myWeatherDbOPerations, "location.xml", id, code);
+              if (Utility.handleCitiesResponse(myWeatherDbOPerations, "location.xml", id, code)) {
+                result = true;
+              }
             } catch (IOException | XmlPullParserException e) {
               e.printStackTrace();
             }
             break;
           case COUNTRY:
             try {
-              Utility.handleCountriesResponse(myWeatherDbOPerations, "location.xml", id, code);
+              if (Utility.handleCountriesResponse(myWeatherDbOPerations, "location.xml", id, code)) {
+                result = true;
+              }
             } catch (IOException | XmlPullParserException e) {
               e.printStackTrace();
             }
             break;
           case PROVINCE:
             try {
-              Utility.handleProvincesResponse(myWeatherDbOPerations, "location.xml");
+              if (Utility.handleProvincesResponse(myWeatherDbOPerations, "location.xml")) {
+                result = true;
+              }
             } catch (IOException | XmlPullParserException e) {
               e.printStackTrace();
             }
@@ -153,8 +220,80 @@ public class ChooseAreaActivity extends Activity {
             // Toast.makeText(this, "数据加载失败!", Toast.LENGTH_SHORT).show();
             break;
           }
+
+          if (result) {
+            // 通过runOnUiThread()回到主线程
+            runOnUiThread(new Runnable() {
+              @Override
+              public void run() {
+                switch (type) {
+                case PROVINCE:
+                  try {
+                    queryProvinces();
+                  } catch (IOException | XmlPullParserException e1) {
+                  }
+                  break;
+                case CITY:
+                  try {
+                    queryCities();
+                  } catch (Exception e) {
+                    e.printStackTrace();
+                  }
+                  break;
+                case COUNTRY:
+                  try {
+                    queryCountries();
+                  } catch (IOException | XmlPullParserException e) {
+                    e.printStackTrace();
+                  }
+                default:
+                  break;
+                }
+              }
+            });
+          }
         }
       }).start();
     }
   }
+
+  private void showProgressDialog() {
+    if (progressDialog == null) {
+      progressDialog = new ProgressDialog(this);
+      progressDialog.setMessage("正在加载...");
+      progressDialog.setCanceledOnTouchOutside(false);
+    }
+    progressDialog.show();
+  }
+
+  private void closeProgressDialog() {
+    if (progressDialog != null) {
+      progressDialog.dismiss();
+    }
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see android.app.Activity#onBackPressed()
+   */
+  @Override
+  public void onBackPressed() {
+    if (currentLevel == LEVEL_COUNTRY) {
+      try {
+        queryCities();
+      } catch (IOException | XmlPullParserException e) {
+        e.printStackTrace();
+      }
+    } else if (currentLevel == LEVEL_CITY) {
+      try {
+        queryProvinces();
+      } catch (IOException | XmlPullParserException e) {
+        e.printStackTrace();
+      }
+    } else {
+      finish();
+    }
+  }
+
 }
