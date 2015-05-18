d1f22895bcbe1168fcb221dedd94d0a3d6f25dc3
JackandRoses <zwf722@163.com>
2015-05-18 22:33:21 +0800

    test ok

--- src/com/zhouwf/myweather/activity/WeatherActivity.java
+++ src/com/zhouwf/myweather/activity/WeatherActivity.java
@@ -3,12 +3,15 @@ package com.zhouwf.myweather.activity;
 import org.json.JSONException;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -17,94 +20,133 @@ import com.zhouwf.myweather.util.HttpCallbackListener;
 import com.zhouwf.myweather.util.HttpUtil;
 import com.zhouwf.myweather.util.Utility;
 
-public class WeatherActivity extends Activity {
-  public static final String defValue = "";
-  private LinearLayout weatehrInfoLayout;
-  /** cityNameText */
-  private TextView cityNameText;
-  /** publishText */
-  private TextView publishText;
-  /** weatherDespText */
-  private TextView weatherDespText;
-  /** temp1Text */
-  private TextView temp1Text;
-  /** temp2Text */
-  private TextView temp2Text;
-  /** currentDateText */
-  private TextView currentDateText;
+public class WeatherActivity extends Activity implements OnClickListener {
+    public static final String defValue = "";
+    private LinearLayout weatehrInfoLayout;
+    /** cityNameText */
+    private TextView cityNameText;
+    /** publishText */
+    private TextView publishText;
+    /** weatherDespText */
+    private TextView weatherDespText;
+    /** temp1Text */
+    private TextView temp1Text;
+    /** temp2Text */
+    private TextView temp2Text;
+    /** currentDateText */
+    private TextView currentDateText;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    requestWindowFeature(Window.FEATURE_NO_TITLE);
-    setContentView(R.layout.weather_layout);
-    // 初始化控件
-    weatehrInfoLayout = (LinearLayout) findViewById(R.id.weather_info_layout);
-    cityNameText = (TextView) findViewById(R.id.city_name);
-    publishText = (TextView) findViewById(R.id.publish_text);
-    weatherDespText = (TextView) findViewById(R.id.weather_desp);
-    temp1Text = (TextView) findViewById(R.id.temp1);
-    temp2Text = (TextView) findViewById(R.id.temp2);
-    currentDateText = (TextView) findViewById(R.id.current_date);
-    String countryCode = getIntent().getStringExtra("country_code");
-    if (!TextUtils.isEmpty(countryCode)) {
-      // 有县级代号就去查询天气
-      publishText.setText("同步中...");
-      weatehrInfoLayout.setVisibility(View.INVISIBLE);
-      cityNameText.setVisibility(View.INVISIBLE);
-      queryWeatherInfo(countryCode);
-    }
-  }
+    private Button switchCityButton;
 
-  /**
-   * 查询 县级天气
-   * 
-   * @param countryCode
-   */
-  private void queryWeatherInfo(String countryCode) {
-    if (!TextUtils.isEmpty(countryCode)) {
-      String address = "http://m.weather.com.cn/data/" + "101" + countryCode + ".html";
-      queryFromServer(address);
-    }
-  }
+    private Button refreshWeatherButton;
 
-  private void queryFromServer(String address) {
-    if (!TextUtils.isEmpty(address)) {
-      HttpUtil.sendHttpRequest(address, new HttpCallbackListener() {
-
-        @Override
-        public void onFinish(String response) throws JSONException {
-          Utility.handleWeatherInfoResponse(WeatherActivity.this, response);
-          runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-              showWeather();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.weather_layout);
+        // 初始化控件
+        weatehrInfoLayout = (LinearLayout) findViewById(R.id.weather_info_layout);
+        cityNameText = (TextView) findViewById(R.id.city_name);
+        publishText = (TextView) findViewById(R.id.publish_text);
+        weatherDespText = (TextView) findViewById(R.id.weather_desp);
+        temp1Text = (TextView) findViewById(R.id.temp1);
+        temp2Text = (TextView) findViewById(R.id.temp2);
+        currentDateText = (TextView) findViewById(R.id.current_date);
+        switchCityButton = (Button) findViewById(R.id.switch_city);
+        refreshWeatherButton = (Button) findViewById(R.id.refresh_weather);
+        String countryCode = getIntent().getStringExtra("country_code");
+        if (!TextUtils.isEmpty(countryCode)) {
+            // 有县级代号就去查询天气
+            publishText.setText("同步中...");
+            weatehrInfoLayout.setVisibility(View.INVISIBLE);
+            cityNameText.setVisibility(View.INVISIBLE);
+            queryWeatherInfo(countryCode);
+        } else {
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+            String weatherCode = prefs.getString("weather_code", "");
+            if (!TextUtils.isEmpty(weatherCode)) {
+                queryWeatherInfo(weatherCode);
             }
-          });
         }
 
-        @Override
-        public void onError(Exception e) {
-          runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-              publishText.setText("同步失败！");
-            }
-          });
-        }
-      });
+        switchCityButton.setOnClickListener(this);
+        refreshWeatherButton.setOnClickListener(this);
     }
-  }
 
-  private void showWeather() {
-    SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-    cityNameText.setText(prefs.getString("city_name", defValue));
-    publishText.setText("今天" + prefs.getString("publish_time", defValue) + "发布");
-    currentDateText.setText(prefs.getString("current_date", defValue));
-    weatherDespText.setText(prefs.getString("weather_desp", defValue));
-    temp1Text.setText(prefs.getString("temp1", defValue));
-    temp2Text.setText(prefs.getString("temp2", defValue));
-    weatehrInfoLayout.setVisibility(View.VISIBLE);
-    cityNameText.setVisibility(View.VISIBLE);
-  }
+    /**
+     * 查询 县级天气
+     * 
+     * @param countryCode
+     */
+    private void queryWeatherInfo(String countryCode) {
+        if (!TextUtils.isEmpty(countryCode) && countryCode.length() <= 6) {
+            String address = "http://m.weather.com.cn/atad/" + "101" + countryCode + ".html";
+            // http://m.weather.com.cn/atad/101010100.html
+            queryFromServer(address);
+        } else {
+            queryFromServer("http://m.weather.com.cn/atad/" + countryCode + ".html");
+        }
+    }
+
+    private void queryFromServer(String address) {
+        if (!TextUtils.isEmpty(address)) {
+            HttpUtil.sendHttpRequest(address, new HttpCallbackListener() {
+
+                @Override
+                public void onFinish(String response) throws JSONException {
+                    Utility.handleWeatherInfoResponse(WeatherActivity.this, response);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            showWeather();
+                        }
+                    });
+                }
+
+                @Override
+                public void onError(Exception e) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            publishText.setText("同步失败！");
+                        }
+                    });
+                }
+            });
+        }
+    }
+
+    private void showWeather() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        cityNameText.setText(prefs.getString("city_name", defValue));
+        publishText.setText(prefs.getString("publish_time", defValue) + "发布");
+        currentDateText.setText(prefs.getString("current_date", defValue));
+        weatherDespText.setText(prefs.getString("weather_desp", defValue));
+        temp1Text.setText(prefs.getString("temp1", defValue));
+        temp2Text.setText(prefs.getString("temp2", defValue));
+        weatehrInfoLayout.setVisibility(View.VISIBLE);
+        cityNameText.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+        case R.id.switch_city:
+            Intent intent = new Intent(this, ChooseAreaActivity.class);
+            intent.putExtra("from_weather_activity", true);
+            startActivity(intent);
+            finish();
+            break;
+        case R.id.refresh_weather:
+            publishText.setText("同步中...");
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+            String weatherCode = prefs.getString("weather_code", "");
+            if (!TextUtils.isEmpty(weatherCode)) {
+                queryWeatherInfo(weatherCode);
+            }
+        default:
+            break;
+        }
+    }
 }
--- src/com/zhouwf/myweather/util/Utility.java
+++ src/com/zhouwf/myweather/util/Utility.java
@@ -34,187 +34,194 @@ import com.zhouwf.myweather.model.WeatherInfo;
  * 
  */
 public class Utility {
-  /**
-   * 省级信息获取及存储
-   * 
-   * @param myWeatherDbOP DB操作类
-   * @param fileName 省市县代号文件名
-   * @return 处理结果(boolean)
-   * @throws IOException
-   * @throws XmlPullParserException
-   */
-  public synchronized static boolean handleProvincesResponse(MyWeatherDbOPerations myWeatherDbOP, String fileName) throws IOException, XmlPullParserException {
-    if (!TextUtils.isEmpty(fileName)) {
-      AssetManager assetManager = ApplicationContext.getContext().getAssets();
-      InputStream is = assetManager.open(fileName);
-      XmlPullParserFactory xmlPullParserFactory = XmlPullParserFactory.newInstance();
-      // xmlPullParserFactory.setNamespaceAware(true);
-      XmlPullParser xmlPullParser = xmlPullParserFactory.newPullParser();
-      xmlPullParser.setInput(is, "utf-8");
-      List<Province> provinceList = new ArrayList<Province>();
-      Province province = null;
-      int eventType = xmlPullParser.getEventType();
-      while (eventType != XmlPullParser.END_DOCUMENT) {
-        if (eventType == XmlPullParser.START_TAG) {
-          if (ProvinceTbFields.TB_NAME.toLowerCase(Locale.US).equals(xmlPullParser.getName())) {
-            province = new Province();
-            province.setProvinceCode(xmlPullParser.getAttributeValue(null, "id"));
-            province.setProvinceName(xmlPullParser.getAttributeValue(null, "name"));
-          }
+    /**
+     * 省级信息获取及存储
+     * 
+     * @param myWeatherDbOP
+     *            DB操作类
+     * @param fileName
+     *            省市县代号文件名
+     * @return 处理结果(boolean)
+     * @throws IOException
+     * @throws XmlPullParserException
+     */
+    public synchronized static boolean handleProvincesResponse(MyWeatherDbOPerations myWeatherDbOP, String fileName) throws IOException, XmlPullParserException {
+        if (!TextUtils.isEmpty(fileName)) {
+            AssetManager assetManager = ApplicationContext.getContext().getAssets();
+            InputStream is = assetManager.open(fileName);
+            XmlPullParserFactory xmlPullParserFactory = XmlPullParserFactory.newInstance();
+            // xmlPullParserFactory.setNamespaceAware(true);
+            XmlPullParser xmlPullParser = xmlPullParserFactory.newPullParser();
+            xmlPullParser.setInput(is, "utf-8");
+            List<Province> provinceList = new ArrayList<Province>();
+            Province province = null;
+            int eventType = xmlPullParser.getEventType();
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                if (eventType == XmlPullParser.START_TAG) {
+                    if (ProvinceTbFields.TB_NAME.toLowerCase(Locale.US).equals(xmlPullParser.getName())) {
+                        province = new Province();
+                        province.setProvinceCode(xmlPullParser.getAttributeValue(null, "id"));
+                        province.setProvinceName(xmlPullParser.getAttributeValue(null, "name"));
+                    }
+                }
+                if (eventType == XmlPullParser.END_TAG) {
+                    if (ProvinceTbFields.TB_NAME.toLowerCase(Locale.US).equals(xmlPullParser.getName())) {
+                        provinceList.add(province);
+                    }
+                }
+                eventType = xmlPullParser.next();
+            }
+            // 存储操作
+            for (Province eachProvince : provinceList) {
+                myWeatherDbOP.saveProvince(eachProvince);
+            }
+            return true;
         }
-        if (eventType == XmlPullParser.END_TAG) {
-          if (ProvinceTbFields.TB_NAME.toLowerCase(Locale.US).equals(xmlPullParser.getName())) {
-            provinceList.add(province);
-          }
-        }
-        eventType = xmlPullParser.next();
-      }
-      // 存储操作
-      for (Province eachProvince : provinceList) {
-        myWeatherDbOP.saveProvince(eachProvince);
-      }
-      return true;
+        return false;
     }
-    return false;
-  }
 
-  /**
-   * 市级信息获取及存储
-   * 
-   * @param myWeatherDbOP DB操作类
-   * @param fileName 省市县代号文件名
-   * @param provinceId 省ID
-   * @param provinceCode 省Code
-   * @return 处理结果(boolean)
-   * @throws IOException
-   * @throws XmlPullParserException
-   */
-  public synchronized static boolean handleCitiesResponse(MyWeatherDbOPerations myWeatherDbOP, String fileName, int provinceId, String provinceCode) throws IOException, XmlPullParserException {
-    if (!TextUtils.isEmpty(fileName)) {
-      AssetManager assetManager = ApplicationContext.getContext().getAssets();
-      InputStream is = assetManager.open(fileName);
-      XmlPullParserFactory xmlPullParserFactory = XmlPullParserFactory.newInstance();
-      // xmlPullParserFactory.setNamespaceAware(true);
-      XmlPullParser xmlPullParser = xmlPullParserFactory.newPullParser();
-      xmlPullParser.setInput(is, "utf-8");
-      List<City> cityList = new ArrayList<City>();
-      City city = null;
-      int eventType = xmlPullParser.getEventType();
-      while (eventType != XmlPullParser.END_DOCUMENT) {
-        if (eventType == XmlPullParser.START_TAG) {
-          // 判断城市ID是否对应省份ID
-          if (CityTbFields.TB_NAME.toLowerCase(Locale.US).equals(xmlPullParser.getName()) && xmlPullParser.getAttributeValue(null, "id").substring(0, 2).equals(provinceCode)) {
-            city = new City();
-            city.setProvinceOfCityId(provinceId);
-            city.setCityCode(xmlPullParser.getAttributeValue(null, "id"));
-            city.setCityName(xmlPullParser.getAttributeValue(null, "name"));
-            cityList.add(city);
-          }
+    /**
+     * 市级信息获取及存储
+     * 
+     * @param myWeatherDbOP
+     *            DB操作类
+     * @param fileName
+     *            省市县代号文件名
+     * @param provinceId
+     *            省ID
+     * @param provinceCode
+     *            省Code
+     * @return 处理结果(boolean)
+     * @throws IOException
+     * @throws XmlPullParserException
+     */
+    public synchronized static boolean handleCitiesResponse(MyWeatherDbOPerations myWeatherDbOP, String fileName, int provinceId, String provinceCode) throws IOException, XmlPullParserException {
+        if (!TextUtils.isEmpty(fileName)) {
+            AssetManager assetManager = ApplicationContext.getContext().getAssets();
+            InputStream is = assetManager.open(fileName);
+            XmlPullParserFactory xmlPullParserFactory = XmlPullParserFactory.newInstance();
+            // xmlPullParserFactory.setNamespaceAware(true);
+            XmlPullParser xmlPullParser = xmlPullParserFactory.newPullParser();
+            xmlPullParser.setInput(is, "utf-8");
+            List<City> cityList = new ArrayList<City>();
+            City city = null;
+            int eventType = xmlPullParser.getEventType();
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                if (eventType == XmlPullParser.START_TAG) {
+                    // 判断城市ID是否对应省份ID
+                    if (CityTbFields.TB_NAME.toLowerCase(Locale.US).equals(xmlPullParser.getName()) && xmlPullParser.getAttributeValue(null, "id").substring(0, 2).equals(provinceCode)) {
+                        city = new City();
+                        city.setProvinceOfCityId(provinceId);
+                        city.setCityCode(xmlPullParser.getAttributeValue(null, "id"));
+                        city.setCityName(xmlPullParser.getAttributeValue(null, "name"));
+                        cityList.add(city);
+                    }
+                }
+                // if (eventType == XmlPullParser.END_TAG) {
+                // if
+                // (CityTbFields.TB_NAME.toLowerCase(Locale.US).equals(xmlPullParser.getName()))
+                // {
+                // cityList.add(city);
+                // }
+                // }
+                eventType = xmlPullParser.next();
+            }
+            // 存储操作
+            for (City eachCity : cityList) {
+                myWeatherDbOP.saveCity(eachCity);
+            }
+            return true;
         }
-        // if (eventType == XmlPullParser.END_TAG) {
-        // if
-        // (CityTbFields.TB_NAME.toLowerCase(Locale.US).equals(xmlPullParser.getName()))
-        // {
-        // cityList.add(city);
-        // }
-        // }
-        eventType = xmlPullParser.next();
-      }
-      // 存储操作
-      for (City eachCity : cityList) {
-        myWeatherDbOP.saveCity(eachCity);
-      }
-      return true;
+        return false;
     }
-    return false;
-  }
 
-  /**
-   * 县级信息获取及存储
-   * 
-   * @param myWeatherDbOP DB操作类
-   * @param fileName 省市县代号文件名
-   * @param cityId 县ID
-   * @return 处理结果(boolean)
-   * @throws IOException
-   * @throws XmlPullParserException
-   */
-  public synchronized static boolean handleCountriesResponse(MyWeatherDbOPerations myWeatherDbOP, String fileName, int cityId, String cityCode) throws IOException, XmlPullParserException {
-    if (!TextUtils.isEmpty(fileName)) {
-      AssetManager assetManager = ApplicationContext.getContext().getAssets();
-      InputStream is = assetManager.open(fileName);
-      XmlPullParserFactory xmlPullParserFactory = XmlPullParserFactory.newInstance();
-      // xmlPullParserFactory.setNamespaceAware(true);
-      XmlPullParser xmlPullParser = xmlPullParserFactory.newPullParser();
-      xmlPullParser.setInput(is, "utf-8");
-      List<Country> countryList = new ArrayList<Country>();
-      Country country = null;
-      int eventType = xmlPullParser.getEventType();
-      while (eventType != XmlPullParser.END_DOCUMENT) {
-        if (eventType == XmlPullParser.START_TAG) {
-          // 判断城市ID是否对应省份ID
-          if (CountryTbFields.TB_NAME.toLowerCase(Locale.US).equals(xmlPullParser.getName()) && xmlPullParser.getAttributeValue(null, "id").substring(0, 4).equals(cityCode)) {
-            country = new Country();
-            country.setCityOfCountryId(cityId);
-            country.setCountryCode(xmlPullParser.getAttributeValue(null, "id"));
-            country.setCountryName(xmlPullParser.getAttributeValue(null, "name"));
-            countryList.add(country);
-          }
+    /**
+     * 县级信息获取及存储
+     * 
+     * @param myWeatherDbOP
+     *            DB操作类
+     * @param fileName
+     *            省市县代号文件名
+     * @param cityId
+     *            县ID
+     * @return 处理结果(boolean)
+     * @throws IOException
+     * @throws XmlPullParserException
+     */
+    public synchronized static boolean handleCountriesResponse(MyWeatherDbOPerations myWeatherDbOP, String fileName, int cityId, String cityCode) throws IOException, XmlPullParserException {
+        if (!TextUtils.isEmpty(fileName)) {
+            AssetManager assetManager = ApplicationContext.getContext().getAssets();
+            InputStream is = assetManager.open(fileName);
+            XmlPullParserFactory xmlPullParserFactory = XmlPullParserFactory.newInstance();
+            // xmlPullParserFactory.setNamespaceAware(true);
+            XmlPullParser xmlPullParser = xmlPullParserFactory.newPullParser();
+            xmlPullParser.setInput(is, "utf-8");
+            List<Country> countryList = new ArrayList<Country>();
+            Country country = null;
+            int eventType = xmlPullParser.getEventType();
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                if (eventType == XmlPullParser.START_TAG) {
+                    // 判断城市ID是否对应省份ID
+                    if (CountryTbFields.TB_NAME.toLowerCase(Locale.US).equals(xmlPullParser.getName()) && xmlPullParser.getAttributeValue(null, "id").substring(0, 4).equals(cityCode)) {
+                        country = new Country();
+                        country.setCityOfCountryId(cityId);
+                        country.setCountryCode(xmlPullParser.getAttributeValue(null, "id"));
+                        country.setCountryName(xmlPullParser.getAttributeValue(null, "name"));
+                        countryList.add(country);
+                    }
+                }
+                // if (eventType == XmlPullParser.END_TAG) {
+                // if
+                // (CountryTbFields.TB_NAME.toLowerCase(Locale.US).equals(xmlPullParser.getName()))
+                // {
+                // countryList.add(country);
+                // }
+                // }
+                eventType = xmlPullParser.next();
+            }
+            // 存储操作
+            for (Country eachCountry : countryList) {
+                myWeatherDbOP.saveCountry(eachCountry);
+            }
+            return true;
         }
-        // if (eventType == XmlPullParser.END_TAG) {
-        // if
-        // (CountryTbFields.TB_NAME.toLowerCase(Locale.US).equals(xmlPullParser.getName()))
-        // {
-        // countryList.add(country);
-        // }
-        // }
-        eventType = xmlPullParser.next();
-      }
-      // 存储操作
-      for (Country eachCountry : countryList) {
-        myWeatherDbOP.saveCountry(eachCountry);
-      }
-      return true;
+        return false;
     }
-    return false;
-  }
 
-  /**
-   * 解析从服务器传来的JSON数据
-   * 
-   * @throws JSONException
-   */
-  public static void handleWeatherInfoResponse(Context context, String response) throws JSONException {
-    JSONObject jsonObject = new JSONObject(response);
-    JSONObject weatherInfo = jsonObject.getJSONObject("weatherinfo");
-    WeatherInfo wInfo = new WeatherInfo();
-    wInfo.setCityname(weatherInfo.getString("city"));
-    wInfo.setWeatherCode(weatherInfo.getString("cityid"));
-    wInfo.setTemp1(weatherInfo.getString("temp1"));
-    wInfo.setTemp2(weatherInfo.getString("temp2"));
-    wInfo.setWeatherDesp(weatherInfo.getString("weather"));
-    wInfo.setPublishTime(weatherInfo.getString("ptime"));
-    // 将服务器的数据保存到sharePreference中
-    saveWeatherInfo(context, wInfo);
+    /**
+     * 解析从服务器传来的JSON数据
+     * 
+     * @throws JSONException
+     */
+    public static void handleWeatherInfoResponse(Context context, String response) throws JSONException {
+        JSONObject jsonObject = new JSONObject(response);
+        JSONObject weatherInfo = jsonObject.getJSONObject("weatherinfo");
+        WeatherInfo wInfo = new WeatherInfo();
+        wInfo.setCityname(weatherInfo.getString("city"));
+        wInfo.setWeatherCode(weatherInfo.getString("cityid"));
+        wInfo.setTemp1(weatherInfo.getString("temp1"));
+        wInfo.setWeatherDesp(weatherInfo.getString("weather1"));
+        wInfo.setPublishTime(weatherInfo.getString("date_y"));
+        // 将服务器的数据保存到sharePreference中
+        saveWeatherInfo(context, wInfo);
 
-  }
+    }
 
-  /**
-   * 将服务器的数据保存到sharePreference中
-   * 
-   * @param wInfo
-   */
-  private static void saveWeatherInfo(Context context, WeatherInfo wInfo) {
-    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy年M月d日", Locale.CHINA);
-    SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(context).edit();
-    editor.putBoolean("city_selected", true);
-    editor.putString("city_name", wInfo.getCityname());
-    editor.putString("weather_code", wInfo.getWeatherCode());
-    editor.putString("temp1", wInfo.getTemp1());
-    editor.putString("temp2", wInfo.getTemp2());
-    editor.putString("weather_desp", wInfo.getWeatherDesp());
-    editor.putString("publish_time", wInfo.getPublishTime());
-    editor.putString("current_date", simpleDateFormat.format(new Date()));
-    editor.commit();
-  }
+    /**
+     * 将服务器的数据保存到sharePreference中
+     * 
+     * @param wInfo
+     */
+    private static void saveWeatherInfo(Context context, WeatherInfo wInfo) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy年M月d日", Locale.CHINA);
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(context).edit();
+        editor.putBoolean("city_selected", true);
+        editor.putString("city_name", wInfo.getCityname());
+        editor.putString("weather_code", wInfo.getWeatherCode());
+        editor.putString("temp1", wInfo.getTemp1());
+        editor.putString("weather_desp", wInfo.getWeatherDesp());
+        editor.putString("publish_time", wInfo.getPublishTime());
+        editor.putString("current_date", simpleDateFormat.format(new Date()));
+        editor.commit();
+    }
 }
